9ba44b7632de6e772191969d09773a80a58e26e4
Saurabh <idkxyz001@gmail.com>
2026-02-25 19:12:22 +0530

    Tag Project

---
+++ Event/25-02-26/script.js
@@ -0,0 +1,54 @@
+const select = document.getElementById("tagType")
+const form = document.querySelector("form")
+const TagName = document.getElementById("tagname")
+const TagText = document.getElementById("tagContent")
+
+function updateSelect() {
+    const main = document.getElementsByTagName("main")[0]
+    const allElements = [...main.children]
+
+    // console.log(allElements)
+    // console.log(allElements[0])
+    // console.log(allElements[1])
+    // console.log(Array.isArray(allElements))
+    allElements.forEach((value) => {
+        const option = document.createElement("option")
+        option.innerText = value.id;
+        option.value = value.id;
+        select.appendChild(option)
+    })
+}
+
+updateSelect()
+
+function addTag() {
+
+
+    const myelement = document.getElementById(select.value)
+
+    console.log({ myelement })
+
+    const newElement = document.createElement(TagName.value)
+    newElement.innerText = TagText.value
+    newElement.id = "el-" + TagText.value.toLowerCase().slice(0, 5)
+    console.log({ newElement })
+
+
+    myelement.appendChild(newElement)
+}
+
+form.addEventListener("submit", (e) => {
+    alert("Okay")
+    e.preventDefault()
+    addTag()
+
+    TagName.value = ""
+    TagText.value = ""
+
+    select.innerHTML =
+        `
+     <option value="open" hidden>Select Tag</option>
+            <option value="main">Main</option>
+    `
+    updateSelect();
+})
