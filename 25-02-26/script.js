94db44ce1e56a2dde0a7779d14ac2853e6ea8269
Saurabh <idkxyz001@gmail.com>
2026-02-25 12:15:11 +0530

    Array

---
+++ 25-02-26/script.js
@@ -0,0 +1,52 @@
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+
+// const arr1 = [101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126, 127, 128, 129, 130];
+
+
+
+// const newArray = arr.concat(arr, arr1)
+
+// const newArray = [arr, arr1].flat(3)
+//...
+// const name = "USERNAME"
+// console.log(...name)
+// const newArray = [...arr, ...arr1, ...name]
+
+
+// function fn(a, b, ...c) {
+//     console.log("A : ", a)
+//     console.log("B: ", b)
+//     console.log("C : ", c)
+// }
+
+
+// fn(12, 213, 34, 45, 34)
+
+
+
+
+
+
+// const newArray = arr.splice(3, 4, 94, 95, 96, 97, 98)
+// const newArray = arr.slice(10, 14)
+
+// const newArray = arr.splice(0, 5)
+
+
+// const newArray = arr.splice(9, 1)
+// const newArray = arr.splice(3, 0, 99)
+// const newArray = arr.splice(3, 1, 99)
+
+
+// const arr = [
+//     true,
+//     () => { console.log("Hello Array") },
+//     23,
+//     "USERNAME",
+//     [1, 2, 3, 4],
+// ]
+
+// // const a = arr[0]
+// // const b = arr[1]
+
+// const [a, b, c, ...d] = [[arr, 12].flat(), 13].flat();
