60e712390cb15ae74f62cb506ae7d80c651f4b38
Saurabh <idkxyz001@gmail.com>
2026-04-07 11:59:41 +0530

    JSON

---
+++ Input/07-04-26/Script.js
@@ -0,0 +1,55 @@
+// console.log(1 + 1)
+// console.log(1 + '1')//11
+// console.log(1 - '4')//-3
+// console.log(2 * '4')//8
+// console.log(2 / '4')//8
+
+
+// console.log(2 * 3 + '1' / 2)
+// console.log(1 + '2' - 4 * 3)
+
+// console.log(10 - '12' + '10' + 8 / 2)
+// console.log(-2 + '10' + 4)
+//12
+//12
+
+// let num = "1200"
+// let a = 12
+// // console.log(typeof (num))
+// num = Number(num)
+// console.log(Number(num) + a)
+// console.log(Number("823642.34"))
+
+// console.log(parseInt("2864.826"))
+// console.log(parseFloat("2864.826"))
+
+
+// console.log(parseInt("36834h83"))
+
+// console.log(Number("true"))
+// console.log(parseInt("true"))
+// console.log(Boolean("true") == true)
+// console.log("true")
+
+// let num = 234123;
+// console.log(String(num))
+// console.log(num.toString())
+
+
+//OBJ -> JSON
+//JSON -> OBJ
+
+// const obj = {
+//     name: "username",
+//     value: "MyValue",
+//     isValid: true,
+//     isObject: true,
+// }
+
+// const JSONOBJ = JSON.stringify(obj)
+// // console.log(JSON.stringify(obj))
+// console.log(JSONOBJ)
+
+// console.log(JSON.parse(JSONOBJ))
+
+
