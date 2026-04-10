ceb425c776d0794cb04dc9a4f552363bcefc4687
Saurabh <idkxyz001@gmail.com>
2026-04-10 14:59:34 +0530

    FNS

---
+++ 02-04 batch/10-04-26/script.js
@@ -0,0 +1,64 @@
+// function fn(a) {
+//     // let b =12;
+//     console.log(a);
+// }
+
+// fn(12)
+
+// // let a = 12
+
+
+// function x() {
+//     console.log("x function")
+// }
+
+// let a = x;
+
+// fn(x);
+
+// const Value =   ()  {
+//     console.log("Hello User")
+// }
+
+
+
+// fn(x)
+// fn(function () {
+//     console.log("Hello User")
+// })
+
+
+// const value = () => {
+//     console.log("Hello JS")
+// }
+
+
+// const sum = (a, b) => a + b
+
+// const pow = a => a ** 2
+
+// const sum = (a, b) => {
+
+//     return a + b
+// }
+
+// ()=>{}
+// ()=>value
+// =>
+
+
+// const fn = a => a => a => a * a
+
+// const ans = fn(12)
+
+// const result = ans()
+
+//IIFE
+
+// (function () {
+//     console.log("Hello IIFE")
+// })();
+
+// (function (a, b) {
+//     console.log(a + b)
+// })(12, 13)
