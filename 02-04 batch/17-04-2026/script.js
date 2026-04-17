ffb7c92ab1c31511cde6e21d2f1b0e3af156ae8e
Saurabh <idkxyz001@gmail.com>
2026-04-17 15:31:28 +0530

    JS Array Functions

---
+++ 02-04 batch/17-04-2026/script.js
@@ -0,0 +1,39 @@
+// const arr = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+//push pop shift unshift length
+// arr.push(6);
+// arr.pop();
+// arr.shift();
+// arr.unshift(0);
+// console.log(arr.length);
+
+// arr.splice(1, 3)
+
+// arr.splice(3, 2)
+
+// arr.splice(5, 0, 6, 99)
+
+// arr.splice(8, 0, 101, 102, 103)
+// const arr = [1, 2, 3, 4, 5, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+
+// arr.splice(5, 3, 79, 78, 77)
+
+
+// arr.reverse()
+
+
+const arr = [2, 3, 4, 1, 8, 5, 9, 7, 6, 11, 22, 33, 44]
+
+arr.sort((a, b) => a - b)
+// arr.sort(xyz)
+
+
+
+// function xyz(a, b) {
+//     return a - b
+// }
+arr.sort((a, b) => b - a)
+
+
+
