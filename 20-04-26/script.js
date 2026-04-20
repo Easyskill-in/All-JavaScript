1f93e60070f7d640146a6bdd4931fb1b5686c24f
Saurabh <idkxyz001@gmail.com>
2026-04-20 11:48:55 +0530

    Callback Hell

---
+++ 20-04-26/script.js
@@ -0,0 +1,27 @@
+// setTimeout(() => {
+//     console.log("Hello Easyskill")
+// }, 4000)
+
+
+// function getData(id, callback) {
+//     setTimeout(() => {
+//         console.log("Data : ", id)
+//         // getData(63)
+//         if (callback) {
+//             callback()
+//         }
+
+//     }, 2000)
+// }
+
+
+// getData(12, () => {
+//     getData(67, () => {
+//         getData(99, () => {
+//             getData(101)
+//         })
+//     })
+// })
+
+// getData(63)
+// getData(99)
