8d91c71b7b8c0681a8d06ad32addd71c7c43f14d
idkxyz001@gmail.com <me.eashaan@gmail.com>
2026-05-04 15:56:22 +0530

    New

---
+++ 02-04 batch/04-05-2026-1/index.js
@@ -0,0 +1,42 @@
+// console.log(Math.pow(2, 3))
+// console.log(Math.pow(2, 12))
+
+// console.log(Math.sqrt(25))//5
+// console.log(Math.sqrt(16))//4
+// console.log(Math.sqrt(15.9))
+// console.log(Math.sqrt(9))//3
+// console.log(Math.sqrt(1))//3
+
+
+// console.log(Math.min(1, 3, 35, 2, 57, 3, 2))
+// console.log(Math.max(1, 3, 35, 2, 57, 3, 2))
+
+// console.log(Math.max())
+// console.log(Math.cbrt(27))
+// console.log(Math.cbrt(Math.pow(25, 3)))
+
+// console.log(Math.pow(25, 3))
+
+
+// const PI = 3.14159;
+
+// console.log(Math.round(3.14))
+// console.log(Math.round(3.61))
+
+// console.log(Math.floor(3.14))
+// console.log(Math.floor(3.99))
+
+// console.log(Math.ceil(3.00000000000000001))
+// console.log(Math.ceil(3.14))
+// console.log(Math.ceil(3.99))
+
+
+// console.log(Math.floor(Math.random() * 10) + 1)//0-1
+
+
+// function MyFn(num) {
+//     return Math.floor(Math.random() * (num + 1))
+// }
+
+console.log(Math.cbrt(28))
+console.log(Math.cbrt(28))
