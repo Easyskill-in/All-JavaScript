8895f43a5e1c107214327a28c157ae16f0d2d44a
idkxyz001@gmail.com <idkxyz001@gmail.com>
2026-05-13 15:34:22 +0530

    DOM

---
+++ 02-04 batch/DOM/13-05-2026/script.js
@@ -0,0 +1,16 @@
+// const HTML_TAG = document.childNodes[0];
+// const Data = HTML_TAG.childNodes
+// const BodyTag = Data[2]
+
+// const h1 = BodyTag.childNodes[1]
+
+// const h1 = document.getElementById("myh1tag")
+
+// const allPTag = document.getElementsByTagName("p")
+
+// const Aclass = document.getElementsByClassName("A")
+
+
+// const h1 = document.querySelector("p")
+// const h1 = document.querySelector("#myh1tag")
+const h1 = document.querySelectorAll(".A")
