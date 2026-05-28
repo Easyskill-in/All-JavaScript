3529ede65ae0a9d262707f0c2a1e1b083f248a77
idkxyz001@gmail.com <idkxyz001@gmail.com>
2026-05-28 09:36:03 +0530

    Loops

---
+++ 08-10 batch/28-05-2026/script.js
@@ -0,0 +1,99 @@
+// ____________ ? ___________ : _______________;
+
+// true ? console.log("Yes") : console.log("No");
+
+// let x;
+// let age = 12;
+
+// x = age >= 18 ? 100 : 10;
+// x = age >= 18 ? 100 : age >= 5 ? 20 : 10;
+
+//+ - ++ --
+
+
+// let a = -12;
+// console.log(-a)
+// console.log(+a)
+
+
+// let a = Number(prompt("Enter A Number"))
+
+// console.log("The Value of a is ", a)
+
+// a += 10;
+// // a = a+10;
+
+// console.log("The Value of a is ", a)
+
+// a -= 3;
+
+// console.log("The Value of a is ", a)
+
+
+
+// a++;//post
+// ++a;//pre
+
+// console.log(a)
+
+// let a = 12;//a=12
+
+// let b = ++a;//a=13 b=13
+
+// let c = b++;//b=14 c=13
+
+// let d = ++b;//b=15 d=15
+
+// console.log("A : ", a);//
+// console.log("B : ", b);//
+// console.log("C : ", c);//
+// console.log("D : ", d);//
+
+
+//while do-while for  } loops type
+
+
+// while () {
+
+// }
+
+//
+// let a;
+// if (a) {
+//     console.log("Yes")
+// } else {
+//     console.log("No")
+// }
+
+// let i = 1;
+
+// do {
+//     console.log("Hello JS", i)
+//     i++;
+// } while (i == 3);
+
+// console.log("Next line", i)
+// let i = 1;
+// while (i <= 3) {
+//     console.log("Hello JS", i)
+//     i++;
+// }
+// console.log("Next line", i)
+
+// let i = 1;
+// while (i <= 10) {
+//     console.log("Hello JS", i)
+//     i++;
+// }
+
+
+let number = 324; 
+
+
+while (number != 0) {
+    console.log(number)
+    number = parseInt(number / 10);
+}
+
+console.log(number)
+
