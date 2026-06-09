10451ddb1a573a935d490f7933fc618eaa145e3a
idkxyz001@gmail.com <idkxyz001@gmail.com>
2026-06-09 09:16:02 +0530

    JS Math Function

---
+++ 08-10 batch/09-06-2026/script.js
@@ -0,0 +1,54 @@
+//Math
+
+// console.log(Math.PI);
+
+
+let a = 3.5286483689365;
+
+// console.log(a)
+// console.log("Round :", Math.round(a))
+// console.log("Floor :", Math.floor(a))
+// console.log("Ceil  :", Math.ceil(a))
+
+// console.log(Math.abs(-6236))
+
+// console.log(Math.min(1, 2, 4, 55, 33, -1))
+// console.log(Math.max(1, 2, 4, 55, 33, -1))
+
+// console.log(Math.pow(2, 16))
+// console.log(Math.sqrt(4))//->2
+// console.log(Math.sqrt(5))
+// console.log(Math.sqrt(6))
+// console.log(Math.sqrt(7))
+// console.log(Math.sqrt(8))
+// console.log(Math.sqrt(9))//->3
+// console.log(Math.sqrt(10))
+// console.log(Math.sqrt(11))
+// console.log(Math.sqrt(12))
+// console.log(Math.sqrt(13))
+// console.log(Math.sqrt(14))
+// console.log(Math.sqrt(15))
+// console.log(Math.sqrt(16))//->4
+
+// console.log(Math.cbrt(Math.pow(7, 3)))
+
+// console.log(Math.cbrt(8))//2
+
+// console.log(Math.cbrt(22))
+
+// console.log(Math.cbrt(27))//3
+
+
+// console.log(Math.random())
+// console.log(Math.floor(Math.random() * 10) + 1)
+
+// 5 - 10;
+
+// let min = 5;
+// let max = 10;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+// fn(12,99)
+// fn(99)
