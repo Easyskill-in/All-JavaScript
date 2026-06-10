def854411332359a720175bbd4a236bdaf2e080c
idkxyz001@gmail.com <idkxyz001@gmail.com>
2026-06-10 09:44:39 +0530

    String in Js

---
+++ 08-10 batch/10-06-2026/script.js
@@ -0,0 +1,97 @@
+// console.log(Math.floor(Math.random() * 100) + 1)
+// let min = 100000;
+// let max = 999999;
+
+// console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
+
+// Math.round(1.4)
+// console.log(Math.floor(1.999999999))
+// console.log(Math.ceil(1.00000001))
+// console.log(Math.trunc(1.9999))
+
+
+// console.log(Math.pow(8,3))
+
+// console.log(Math.sqrt(256))
+// console.log(Math.cbrt(8))
+// console.log(Math.cbrt(64))
+
+
+/*
+https://dawn-shake-eff.notion.site/Notes-on-js-Object-part-1-8132a5312b4d41e0861af71d43fad536?pvs=73
+
+https://dawn-shake-eff.notion.site/JavaScript-Object-Methods-98ec7894f8034aa4be1b0f87003ef28f
+
+https://pool-collision-da5.notion.site/JavaScript-String-Functions-Methods-fa2df459a6c5496bbeabb11b95b8e453?pvs=73
+
+https://pool-collision-da5.notion.site/JavaScript-Date-Functions-e889e946da9f4582bbb8e55b5ef5fc8b?pvs=73
+
+https://dawn-shake-eff.notion.site/JavaScript-Window-Events-b7ad5e9e02dc493cb8d93dd967ff9103
+
+https://pool-collision-da5.notion.site/JS-Arrays-fa3724fea8c04748a7a0a89a29b5182c?pvs=143
+
+
+https://pool-collision-da5.notion.site/JavaScript-Date-Functions-e889e946da9f4582bbb8e55b5ef5fc8b
+
+https://strong-people-4ee.notion.site/Mongoose-Query-Operators-Complete-Guide-0a1fb950d88d41568b591c5d51e473ec?pvs=73
+
+https://strong-people-4ee.notion.site/Mongoose-Query-Operators-Complete-Guide-0a1fb950d88d41568b591c5d51e473ec?pvs=73
+
+https://strong-people-4ee.notion.site/Mongoose-Data-Modeling-c3222864e8434ce4a8585ca540eaddfc
+
+https://strong-people-4ee.notion.site/Mongoose-CRUD-Operations-e6e19dd904dd4e5ca36d9ea4bddcd752?pvs=73
+
+https://efficacious-locust-fe5.notion.site/SQL-CRUD-Commands-961278f997824881b244b3bbf79eb958
+
+*/
+
+
+let name = "Easyskillz"
+
+// console.log(name.length)
+// console.log(name.charAt(5))
+
+
+// console.log(name.charCodeAt("9"))
+// String.fromCharCode()
+
+// You don't need to memorize the entire table, but these exact sequences are critical:
+// Digits (0-9): 48 - 57
+// Uppercase (A-Z): 65 - 90
+// Lowercase (a-z): 97 - 122
+
+// let str = "I love JavaScript";
+
+// console.log(str.indexOf("l"))
+// console.log(str.indexOf("z"))
+
+// console.log(str.indexOf("a"))
+// console.log(str.lastIndexOf("a"));
+// console.log(str.includes("Java"));
+
+
+// console.log(str.indexOf(" J"))
+// console.log(str.indexOf("ove  "))
+
+// console.log(str.indexOf("vas"))
+// console.log(str.indexOf("vaS"))
+
+// console.log(str.includes("I love  "))
+
+// let str = "I love JavaScript";
+
+// console.log(str.startsWith("I love J"))
+// console.log(str.endsWith("@gmail.com"))
+
+
+
+
+let msg = "JavaScript";
+console.log(msg.slice(0, 4));       // "Java"
+
+
+
+
+
+
