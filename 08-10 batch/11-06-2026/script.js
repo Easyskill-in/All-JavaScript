bb09780ca8caed75680260b633f4f1fdaec44890
idkxyz001@gmail.com <idkxyz001@gmail.com>
2026-06-11 09:20:22 +0530

    String........

---
+++ 08-10 batch/11-06-2026/script.js
@@ -0,0 +1,43 @@
+// let count = 0;
+// var numberOfSteps = function (num) {
+
+//     if (num === 0) {
+//         return count
+//     }
+//     count++;
+
+//     return num % 2 === 0 ? numberOfSteps(num / 2) : numberOfSteps(num - 1);
+
+// };
+
+
+// // console.log(numberOfSteps(14))
+// console.log(numberOfSteps(123))
+
+// let text2 = "hello world";
+
+// console.log(text2.toUpperCase());   
+
+
+// let password = "Easyskill"
+
+// let str = "Hello world"
+
+
+// // console.log(str.replace("world", "java"))
+// console.log(str.replaceAll("l", "I"))
+
+
+
+// let a = "Hello";
+// let b = "World";
+
+
+// console.log(a.concat(" ", b, " !"))
+
+
+let min = "0000"
+
+// console.log(min.padStart(10, "-"))
+console.log(min.padEnd(10, "*").padStart(min.length + 12, "*"))
+
