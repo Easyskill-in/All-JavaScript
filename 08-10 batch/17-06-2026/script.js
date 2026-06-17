41de68fc7a04644e5b9b0f8c7708f1c4b5a0b811
idkxyz001@gmail.com <idkxyz001@gmail.com>
2026-06-17 09:38:21 +0530

    Array Functions

---
+++ 08-10 batch/17-06-2026/script.js
@@ -0,0 +1,32 @@
+
+
+// let ans = arr
+//     .map(v => v / 2)
+//     .map(v => Math.floor(v))
+//     .map((v) => { return v * 13 })
+//     .filter((v) => v % 2 == 0 && v > 0)
+//     .forEach((v) => {
+//         console.log(v % 12)
+//     }) 
+
+
+// const pow = (x)=>{
+//     return x*x
+// }
+// const pow = x => x * x
+
+let arr = [1, 8, 2, 3, 4, 9, 7]
+// let ans = arr.reduce((acc, value) => {
+//     console.log("Acc : ", acc)
+//     console.log("Value : ", value)
+
+//     return acc * value
+// })
+
+// const ans = arr.some((v) => v % 3 == 0)
+// const ans = arr.every((v) => 0 % v == 0)
+
+
+// const ans = arr.find((v) => {
+//     return (v * 13) % 2 == 0
+// })
