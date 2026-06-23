efb0c365d6cde700c951ec1c8ea7a3737725f653
idkxyz001@gmail.com <idkxyz001@gmail.com>
2026-06-23 09:28:15 +0530

    Object

---
+++ 08-10 batch/23-06-2026/script.js
@@ -0,0 +1,28 @@
+let arr = [1, 2, 3, 4, 5]
+
+const password = "Easyskill@2024"
+
+let obj = {
+    name: "Easyskill",
+    age: 55,
+    marks: 99.9,
+    isActive: true,
+    "user data": [1, 2, 3, 4, 5],
+    xyz: {
+        key: "value"
+    },
+    name: "MyName",
+    // password: password,
+    password
+}
+
+
+
+// obj.age = 12
+// let name = "age"
+// console.log(obj["name"])//
+// console.log(obj[name])//
+// console.log(obj.marks, obj.age)
+
+
+// let a = 99;
