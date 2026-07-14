128e9cfd91e72d1b027e5031c248b5b26f7c8ece
idkxyz001@gmail.com <idkxyz001@gmail.com>
2026-07-14 15:42:38 +0530

    Boxs

---
+++ New 02-04 Batch/14-07-2026/script.js
@@ -0,0 +1,45 @@
+// // alert("Message")
+
+// // const Data = confirm("are you sure?")
+
+// // const Data = prompt("Enter Your Name")
+
+
+// // const num = prompt("Enter Data")
+
+
+// // console.log(num == 12)
+// // console.log(num === 12)
+// // === ,  !==
+
+// // console.log(12 !== '12')
+
+
+// // let a = 4;
+// // let b = '10';
+
+// // console.log(a + b)
+// // console.log(b + a)
+// // console.log(b - a)
+
+
+// // console.log('10' + '10')
+// // console.log('10' + 10)
+
+// // console.log('10' - '10')
+// // console.log('10' - 10)
+
+// // console.log('3' * '2')
+// // console.log(3 * '4')
+
+// // console.log('12' / '2')
+// // console.log(28 / '4')
+
+
+// let a = parseInt(prompt("Enter a Number"))
+// let b = parseInt(prompt("Enter a Number"))
+
+// // console.log(`The Sum of ${a} and ${b} is ${Number(a) + Number(b)}`)
+// console.log(`The Sum of ${a} and ${b} is ${a + b}`)
+
+
